402879b21d1d15dbbab154a68a4b16c9aae56c1d
17043003 <ishizuka533725@gmail.com>
2021-11-12 02:19:25 +0900

    add: authentication using axios and error message is shown.

--- src/react-attendance-manager/src/Login.tsx
+++ src/react-attendance-manager/src/Login.tsx
@@ -1,5 +1,6 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form"
+import axios from "./utils/axios"
 
 type loginFormInput = {
     email: string,
@@ -8,10 +9,25 @@ type loginFormInput = {
 
 const Login = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit: SubmitHandler<loginFormInput> = data => console.log(data);
+    const [errorMessage, setErrorMessage] = useState<string>("");
+
+    const onSubmit: SubmitHandler<loginFormInput> = data => {
+        console.log(data);
+        const params = new URLSearchParams();
+        params.append("email", data.email);
+        params.append("password", data.password);
+        axios.post("/login", params)
+        .then(response => {
+            console.log(response.status);
+        })
+        .catch(() => {
+            setErrorMessage("ログインに失敗しました");
+        })
+    }
     return(
         <Fragment>
             <form onSubmit={handleSubmit(onSubmit)}>
+                <div>{errorMessage}</div>
                 <label>Email</label>
                 <input {...register("email", { 
                     required: true, pattern: /^[a-zA-Z0-9_+-.]+@[a-zA-Z0-9-]+\.[a-zA-Z]+$/i 
